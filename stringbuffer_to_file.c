48c7451851abf75a16430e07a7b9e4015c09e694
daniel-savescu <71008399+daniel-savescu@users.noreply.github.com>
2020-09-09 13:44:01 +0300

    Update stringbuffer_to_file.c

--- stringbuffer_to_file.c
+++ stringbuffer_to_file.c
@@ -1,5 +1,4 @@
-/*C program to create a file and write data into file. */
-
+/*Write String Buffer in C file */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,19 +6,16 @@
 #define BUFFER_SIZE 100
 
 void main(){
-
+    
     char buffer[BUFFER_SIZE];
-
-
     FILE* pointerToFile = fopen("path and file_name", "w");
-
+    
     if(pointerToFile == NULL){
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-
-
-    printf("Enter contents to store in file : \n");
+    
+    rintf("Enter contents to store in file : \n");
     fgets(buffer, BUFFER_SIZE, stdin);
 
     fputs(buffer, pointerToFile);
@@ -27,5 +23,4 @@ void main(){
     fclose(pointerToFile);
 
     printf("File created and saved successfully. :-) \n");
-
 }
