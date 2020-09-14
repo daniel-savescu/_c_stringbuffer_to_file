7cf69e090437bb85b1ea73f05464c16f41250f81
Daniel S <71008399+daniel-savescu@users.noreply.github.com>
2020-09-14 13:36:35 +0300

    Update _c_stringbuffer_to_file.c

--- _c_stringbuffer_to_file.c
+++ _c_stringbuffer_to_file.c
@@ -3,24 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 100
+#define bufferSize 100
+
+void writeToFile(char* stringBuffer, FILE* file);
 
 int main(){
 
- char buffer[BUFFER_SIZE];
+ char buffer[bufferSize];
 
- FILE* pointerToFile = fopen( /*path and file name*/, "w");
+ FILE* pointerToFile = fopen( "filename", "w");
 
  if(pointerToFile == NULL){
     printf("Unable to create file.\n");
     exit(EXIT_FAILURE);
   }
   else{
-    printf("Enter contents to store in file : \n");
-    fgets(buffer, BUFFER_SIZE, stdin);
-    fputs(buffer, pointerToFile);
-    fclose(pointerToFile);
-    }
-  printf("File created and saved successfully. :-) \n");
-  return 0;
+    writeToFile(buffer, pointerToFile);
+  }
+return 0;
+}
+
+void writeToFile(char* stringBuffer, FILE* file){
+
+ printf("Enter contents to store in file : \n");
+
+ fgets(stringBuffer, bufferSize, stdin);
+ fputs(stringBuffer, file);
+ fclose(file);
+
+ printf("File created and saved successfully. :-) \n");
+
 }
